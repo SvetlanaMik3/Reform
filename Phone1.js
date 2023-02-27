5b43aa22a8462c991900e69bde7dfb0b40ebb66c
Svetlana <120339130+SvetlanaMik3@users.noreply.github.com>
2023-02-28 03:24:06 +0400

    Add files via upload

---
+++ Phone1.js
@@ -0,0 +1,9 @@
+function ValidPhone() {
+    var re = /^[\+7|8][\(]?\d{3}[\)]?\d{7}$/;
+    var myPhone = document.getElementById('phone').value;
+    var valid = re.test(myPhone);
+    if (valid) output = 'Номер телефона введен правильно!';
+    else output = 'Номер телефона введен неправильно!';
+    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+    return valid;
+}  
