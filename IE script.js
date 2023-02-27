9033d96aa01f49ae2511fb00c6ae269b8c31bb65
Svetlana <120339130+SvetlanaMik3@users.noreply.github.com>
2023-02-28 03:24:56 +0400

    Add files via upload

---
+++ IE script.js
@@ -0,0 +1,8 @@
+function isInternetExplorer() {
+    return window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+}
+console.log(isInternetExplorer());
+if (isInternetExplorer() === false) 
+{ alert('Допустимые Браузеры'); }
+else {
+ alert('Вы пользуетесь не тем браузером'); }
